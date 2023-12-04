88f0e394b5b953fd5aaa356e490c535ff16c9b5f
robinyeon <robin.yeon@gmail.com>
2023-12-04 09:40:32 +0900

    install: helmet

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { Express } from "express";
+import dotenv from "dotenv";
+import helmet from "helmet";
 import userRouter from "./routes/user.router";
 import { myDataSource } from "./data-source";
 import { errorMiddleware } from "./middleware/error.middleware";
-import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -16,6 +17,7 @@ const app: Express = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(helmet());
 
 app.use("/user", userRouter);
 app.use(errorMiddleware);
