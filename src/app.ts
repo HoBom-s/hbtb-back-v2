3f47fee301572baa747afcc5791efb7ba44513ef
robinyeon <robin.yeon@gmail.com>
2023-12-04 10:07:36 +0900

    install: cors

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { Express } from "express";
 import dotenv from "dotenv";
 import helmet from "helmet";
+import cors from "cors";
 import userRouter from "./routes/user.router";
 import { myDataSource } from "./data-source";
 import { errorMiddleware } from "./middleware/error.middleware";
@@ -14,10 +15,15 @@ myDataSource
   .catch((error) => console.warn(error));
 
 const app: Express = express();
+// const corsOptions = {
+//   origin: "http://example.com",
+//   credentials: true,
+// };
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(helmet());
+// app.use(cors(corsOptions));
 
 app.use("/user", userRouter);
 app.use(errorMiddleware);
