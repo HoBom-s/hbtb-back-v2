999f08bd59c33f2e59e48b7599edd80c2b3fac3e
robinyeon <robin.yeon@gmail.com>
2024-01-17 11:35:44 +0900

    feat: category entity

---
+++ src/routes/category.router.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+const categoryRouter = Router();
+const categoryController = new CategoryController();
+
+// categoryRouter.get("/", categoryController.)
+
+/*
+get /
+post /create
+patch /update
+delete /:id
+*/
