60744ffcfdfaaf780fee216928efeb73b86149cf
robinyeon <robin.yeon@gmail.com>
2024-02-22 17:51:13 +0900

    WIP: category response dto

--- src/dtos/user.dto.ts
+++ src/dtos/user.dto.ts
@@ -1,16 +1,5 @@
+import BaseResponseDto from "./common.dto";
 import User from "../entities/user.entity";
-
-export class BaseResponseDto<T> {
-  body: T;
-
-  constructor(body: T) {
-    this.body = body;
-  }
-
-  toResponse() {
-    return this.body;
-  }
-}
 
 export class UserWithoutPasswordResponseDto extends BaseResponseDto<User> {
   constructor(body: User) {
