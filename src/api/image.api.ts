0685334801e165ec6665be3747627f8df8996cd1
robinyeon <robin.yeon@gmail.com>
2024-03-04 16:27:22 +0900

    fix: changed axios baseURL

--- src/api/image.api.ts
+++ src/api/image.api.ts
@@ -4,8 +4,7 @@ import { config } from "dotenv";
 config();
 
 const axiosInstance = axios.create({
-  // baseURL: process.env.IMAGE_HOST,
-  baseURL: "http://127.0.0.1:3000/",
+  baseURL: process.env.IMAGE_HOST,
   timeout: 3000,
   headers: { "Content-Type": "application/json" },
 });
