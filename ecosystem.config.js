7d20cbae19fe99923a5679bd3817e8e9bab175e4
robinyeon <robin.yeon@gmail.com>
2024-03-22 11:18:20 +0900

    fix: trial_scripts to pm2-runtime

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,14 +2,14 @@ module.exports = {
   apps: [
     {
       name: "hbtb-v2",
-      script: "dist/app.js",
+      script: "./dist/app.js",
       // watch: true,
       // ignore_watch: ["node_modules", "logs", "dist"],
-      // exec_mode: "cluster",
-      // instances: 0,
       // wait_ready: true,
       // listen_timeout: 5000,
       // kill_timeout: 3000,
+      // exec_mode: "cluster",
+      // instances: 0,
     },
   ],
 };
